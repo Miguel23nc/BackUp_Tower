546d3e585b48da89d7212d5c5e23074c654b2993
Mateo2508 <jorgeatomedina2508@gmail.com>
2024-08-07 08:27:36 -0500

    solucion

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -20,6 +20,7 @@ router.post("/login", login)
 router.post("/logout", logout)
 router.post("/postRole", postRole)
 router.post("/postPermissions", postPermissions)
+
 router.get("/getRoles", getRoles)
 router.get("/getPermissions", getPermissions)
 router.get("/profile", validationToken, profile)
