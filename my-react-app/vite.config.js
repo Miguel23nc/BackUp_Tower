5a97ad0220fcdbb60394c35473618c8e548f2a4c
Miguel23nc <1392023@senati.pe>
2025-01-29 08:33:21 -0500

    1.8.1

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   cacheDir: ".vite-cache",
   build: {
     outDir: "dist",
   },
-  server: {
-    historyApiFallback: true,
-  },
+  base: "/", // <- Asegura que las rutas sean absolutas
 });
