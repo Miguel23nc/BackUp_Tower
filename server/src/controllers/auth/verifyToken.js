396a849c9009f8a64c14bdcd72567da64c4318ff
Miguel23nc <1392023@senati.pe>
2025-01-29 10:25:50 -0500

    1.8.4

--- server/src/controllers/auth/verifyToken.js
+++ server/src/controllers/auth/verifyToken.js
@@ -5,28 +5,29 @@ const { JWT_SECRET } = process.env;
 const verifyToken = async (req, res) => {
   const { token } = req?.cookies;
   try {
-    if (!token) {
+    if (token) {
+      jwt.verify(token, JWT_SECRET, async (err, user) => {
+        if (err) {
+          if (err.name === "TokenExpiredError") {
+            return res.status(401).json({ message: "Token expirado" });
+          }
+          return res.status(403).json({ message: "Token no válido" });
+        }
+        const userFound = await User.findOne({ email: user?.email });
+        if (!userFound) {
+          return res
+            .status(401)
+            .json({ message: "No se encuentra este usuario" });
+        }
+        const userData = userFound.toObject();
+        delete userData.password;
+        return res.status(200).json(userData);
+      });
+    } else {
       return res.status(401).json({
         message: "No hay token ",
       });
     }
-    jwt.verify(token, JWT_SECRET, async (err, user) => {
-      if (err) {
-        if (err.name === "TokenExpiredError") {
-          return res.status(401).json({ message: "Token expirado" });
-        }
-        return res.status(403).json({ message: "Token no válido" });
-      }
-      const userFound = await User.findOne({ email: user?.email });
-      if (!userFound) {
-        return res
-          .status(401)
-          .json({ message: "No se encuentra este usuario" });
-      }
-      const userData = userFound.toObject();
-      delete userData.password;
-      return res.status(200).json(userData);
-    });
   } catch (error) {
     console.log("error");
     return res.status(500).json({ message: error });
